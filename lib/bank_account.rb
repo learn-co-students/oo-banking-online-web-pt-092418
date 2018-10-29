9f07ed7bb307524894c593635241f7a42c2fe6f8
Carolyn Whelpley <carolynwhelpley@Jeffs-MacBook-Pro-3.local>
2018-10-29 12:20:48 -0400

    Interim checkin

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
+attr_accessor :balance, :status, :amount
+attr_reader :name
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @amount = amount
+  end
 
+  def deposit(amount)
+    @balance << amount
+  end
+
+  def display_balance
+    p "Your balance is $#{@balance}."
+  end
+
+  def valid
+  end
+
+  def close_account
+    
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :transfer, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @transfer = transfer
+    @status = "pending"
+    @receiver = receiver
+    @amount = amount
+  end
+
+  def valid 
+
+  
+  end
+
+  def execute_transaction
+  
+  end
+
+  def reverse_transfer
+  end
+
+
 end
